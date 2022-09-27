155f7efbbe0003b6538d858a22d016e66c413db6
Glitch0320 <100811040+Glitch0320@users.noreply.github.com>
2022-09-27 01:22:57 -0500

    javascript complete, now to add 5 days to the dom

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,9 @@
 
 // API Weather is updated daily at 1, 4, 7, and 10 am, and 1, 4, 7, and 10pm
 
-const days = [];
-// 26th
-let today = '';
+const forecast = [];
+// 270
+const today = moment().format('DDDD');
 
 function findMost(arr) {
     // Return the item found most in an array, if tie, returns first
@@ -26,8 +26,7 @@ function findMost(arr) {
 }
 
 class day {
-    constructor (initHour, description, temperature, humidity, wind) {
-        this.initHour = initHour;
+    constructor (description, temperature, humidity, wind) {
         this.description = description;
         this.temperature = temperature;
         this.humidity = humidity;
@@ -42,15 +41,16 @@ fetch(`https://api.openweathermap.org/data/2.5/weather?q=Fairmont,mn,us&APPID=3c
 .then( block => {
 
     // date = September 
-    today = moment.unix(block.dt).format('Do');
     const initTime = moment.unix(block.dt).format('MMMM Do YYYY hh:mm:ss a');
     const description = block.weather[0].main;
     const temperature = block.main.temp;
     const humidity = block.main.humidity;
     const wind = block.wind.speed;
     const obj = new day(initTime, description, temperature, humidity, wind);
-    days.push(obj);
-    console.log(days[0])
+
+    // display to dom
+
+
 });
 
 // FORECAST
@@ -58,28 +58,60 @@ fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Fairmont,mn,us&APPID=3
 .then( response => response.json() )
 .then( data => {
 
-    // current day as in 276 or something
-    const today = parseInt(moment.unix(data.list[0].dt).format('DDDD'));
-
-    let averages = {
+    let avgIndex = 0;
+    let temp = {
         descriptions: [],
         temperature: 0,
         humidity: 0,
-        wind: 0
+        wind: 0,
     }
 
+    // this will start at index 271 for example
+    currentDay = parseInt(today) + 1;
+
     // For each day(multiple blocks) average all values for that day then add to days array
-    data.list.forEach( (threeHour) => {
+    data.list.forEach( (increment, i) => {
+
+        incrementDay = parseInt(moment.unix(increment.dt).format('DDDD'))
 
         // current weather doesn't need to be included
-        if ( moment.unix(threeHour.dt).format('Do') != today ) {
+        if ( incrementDay != today ) {
 
-            // Weather is updated daily at 1, 4, 7, and 10 am, and 1, 4, 7, and 10pm
-            let initHour = moment.unix(threeHour.dt).format('MMMM Do YYYY hh a');
-            console.log(initHour)
+            // current day will = 270 until incrementday moves to 271, then currentday = 271
+            if ( incrementDay === currentDay ) {
+
+                temp.descriptions.push(increment.weather[0].main);
+                temp.temperature += increment.main.temp;
+                temp.humidity += increment.main.humidity;
+                temp.wind += increment.wind.speed;
+                avgIndex++;
+
+            } else {
+
+                // average the last day and add it to days, then add this increment to temp, then add one to currentday
+                description = findMost(temp.descriptions);
+                temperature = temp.temperature / avgIndex;
+                humidity = temp.humidity / avgIndex;
+                wind = temp.wind / avgIndex;
+                let averageOfToday = new day(description, temperature, humidity, wind);
+                forecast.push(averageOfToday);
+
+                temp = {
+                    descriptions: [],
+                    temperature: 0,
+                    humidity: 0,
+                    wind: 0,
+                }
+
+                currentDay++;
+                avgIndex = 0;
+
+            }
 
         }
 
     });
+
+    console.log(forecast)
 
 });
